d9a3bd04c02625c4b34e7f4ec595126a6b392124
Manasvi Sharma <manasvisharma821@gmail.com>
2021-09-08 09:08:08 +0530

    Update Main.java

--- Java Basics/abstract_keyword/Main.java
+++ Java Basics/abstract_keyword/Main.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package abstract_keyword;
 
-/**
- *
- * @author manasvi
- */
 interface Key{
     
     void print();
@@ -54,7 +44,7 @@ public class Main extends Word {
 //        Word obj = new Word();
     }
 
-    @Override
+
     void call() {
         
         System.out.println("Abstract call method");
