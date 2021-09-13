b02ccb552f0e74cb7f089fe06d04634cd052a566
Manasvi Sharma <manasvisharma821@gmail.com>
2021-09-13 10:40:35 +0530

    Update CollectionFramework.java

--- Java Basics/collection_framework/CollectionFramework.java
+++ Java Basics/collection_framework/CollectionFramework.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package collection_framework;
+// package collection_framework;
 
 //import java.util.*;
 
