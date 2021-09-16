3c163924c439fa76cd843e41fac76acb3110327c
Manasvi Sharma <manasvisharma821@gmail.com>
2021-09-16 22:39:19 +0530

    Update ExceptionHandling.java

--- Java Basics/exception_handling/ExceptionHandling.java
+++ Java Basics/exception_handling/ExceptionHandling.java
@@ -9,6 +9,29 @@
  *
  * @author manasvi
  */
+
+
+class MyException extends Exception{
+
+    
+//    MyException(){
+//    
+//    }
+    
+//    private String message;
+//    MyException(String m){
+//    
+//        message = m;
+//    }
+    
+//   public String toString(){
+//    
+//       return message;
+//    
+//   }
+    
+    
+}
 public class ExceptionHandling {
 
     /**
@@ -88,6 +111,112 @@ public class ExceptionHandling {
         }
 
     }
+    
+    
+    static void ex4(){
+   
+        try{
+            String a=null;
+            String b = a+" ";
+            
+            throw new NullPointerException();
+//            throw new ArithmeticException();
+        }
+        catch(NullPointerException e){
+        
+            System.out.println(e);
+        }
+    
+    }
+    
+    static void ex5(){
+        
+        try{
+            ex5_1();
+        }
+        catch(ArithmeticException e){           
+           System.out.println(e);       
+        }
+    
+    }
+    
+    static void ex5_1(){
+    
+        try{
+            throw new ArithmeticException();   
+        }
+        catch(ArithmeticException e){
+        
+            System.out.println(e);
+            throw e;
+        }
+    
+    }
+    
+    static void ex6(){
+    
+        try{
+          ex_6();
+        }
+        catch(ArithmeticException | ArrayIndexOutOfBoundsException | NullPointerException e){
+            System.out.println(e);
+        }
+        
+    }
+    
+    static void ex_6() throws ArithmeticException, NullPointerException, ArrayIndexOutOfBoundsException{
+
+        throw new ArithmeticException();
+    
+    }
+    
+    
+    static void ex7(int i){
+    
+        try{
+            
+            if(i==1){
+            
+                throw new Exception();
+            }        
+            System.out.println("All statements in 'Try' are executed ");
+        
+        }
+        catch(Exception e){
+            
+            System.out.println("We are in 'Catch'");
+            System.out.println(e);
+    
+        } 
+        finally{
+        
+            System.out.println("'Finally' will execute regardless of anything ");
+        }
+        
+        
+        
+        
+    }
+    
+    static void ex8(){
+        
+        try {
+                
+                throw new MyException();
+//                throw new MyException("Wishful thinking");
+                
+    
+        }catch(MyException e){
+        
+            System.out.println(e);
+        
+        }
+        
+        
+    
+    }
+    
+    
 
     public static void main(String[] args) {
         // TODO code application logic here
@@ -95,7 +224,15 @@ public class ExceptionHandling {
 //        ex0();
 //        ex1();
 //        ex2();
-        ex3(2);
+//        ex3(2);
+//        ex4();
+//        ex5();
+//        ex6();
+//        ex7(10);
+        ex8();
+        
+
+
 
     }
 
